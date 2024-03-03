cbd858109e3a96e8cec2af5444e7ce1de85ba325
Clément Rieuneau <clem.rieuneau@gmail.com>
2024-03-03 22:26:05 +0100

    CSS Page Accueil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
+import backgroundImage from './../images/PhotoMoi.png';
+
 const Home = () => {
     return (
         <>
@@ -8,7 +10,15 @@ const Home = () => {
                 <Navbar />
             </header>
             <body>
-                <h1> Accueil </h1>
+                <h1 className='title'> Accueil </h1>
+                    <div className="homeContent">
+                        <p> Portfolio BUT Informatique 2021 - 2024 </p>
+                        <p> RIEUNEAU Clément </p>
+                    </div>
+
+                <div className="homeBGImage">
+                    <img className='image' src={backgroundImage} />
+                </div>
             </body>
         </>
     );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Projects = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <header>
+                <Navbar />
+            </header>
+            <body>
+                <h1 className='title'> Mes Projets </h1>
+            </body>
+        </>
     );
 };
 
